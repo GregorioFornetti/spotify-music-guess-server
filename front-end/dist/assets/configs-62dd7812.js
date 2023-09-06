01375d57fe76ce7520df377c2119e85e15cccea9
Rafael Barbeta <rafa.barbeta@gmail.com>
2023-09-06 15:40:18 -0300

    corrige os caminhos para adequação no servidor

--- front-end/dist/assets/configs-62dd7812.js
+++ front-end/dist/assets/configs-62dd7812.js
@@ -1,1 +1,1 @@
-const t="http://localhost:3000/home.html",o="http://localhost:3000/login.html";export{t as l,o as r};
+const t="http://localhost:4000/spotify-music-guess/home.html",o="http://localhost:4000/spotify-music-guess/login.html";export{t as l,o as r};
