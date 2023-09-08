1c1b4eb80de30ec56a8708f2ca57c411da311e4f
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-09-08 00:45:05 -0300

    dist...

---
+++ front-end/dist/spotify-music-guess/assets/configs-48bf7a92.js
@@ -0,0 +1,1 @@
+const o="/spotify-music-guess",s=`${o}/home.html`,t=`${o}/login.html`;export{o as a,s as l,t as r};
--- front-end/dist/spotify-music-guess/assets/configs-64ab1582.js
+++
@@ -1,1 +0,0 @@
-const s="/spotify-music-guess/home.html",i="/spotify-music-guess/login.html";export{s as l,i as r};
