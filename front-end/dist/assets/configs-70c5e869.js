0015106fe6ffe212ccda3785ed13409b5e10c149
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-09-08 00:27:14 -0300

    mudanças forma de build

--- front-end/dist/assets/configs-70c5e869.js
+++
@@ -1,1 +0,0 @@
-const o="",r=`${o}/home.html`,t=`${o}/login.html`;export{r as l,t as r};
---
+++ front-end/dist/spotify-music-guess/assets/configs-64ab1582.js
@@ -0,0 +1,1 @@
+const s="/spotify-music-guess/home.html",i="/spotify-music-guess/login.html";export{s as l,i as r};
